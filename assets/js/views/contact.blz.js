c9a8e0fe8c08d2dcf6a8616b1fb94ec51392e2b4
Tyler Ruff <hello@blazed.space>
2023-05-21 05:45:32 -0400

    Update contact.blz.js

--- assets/js/views/contact.blz.js
+++ assets/js/views/contact.blz.js
@@ -35,14 +35,14 @@ function contact(){
                 </span>
             </a>
 
-            <a href="mailto:hello@blazed.space" class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
+            <a href="mailto:hello@ruff-manage.com" class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
                 <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
 
                 <span class="mt-2">
-                    hello@blazed.space
+                    hello@ruff-manage.com
                 </span>
             </a>
         </div>
